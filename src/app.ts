21f09ae41aecef465e53c26517d4dea282ca88ac
fuadhossainnishad <fuadhossainbk01@gmail.com>
2025-05-12 20:02:36 +0600

    update server logs, database config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,24 @@
 import express from "express";
 import cors from "cors";
+import router from "./router";
+import helmet from "helmet";
+import config from "./app/config";
 
 const app = express();
 
-app.use(cors());
+app.use(helmet())
+app.use(cors({
+    origin: process.env.NODE_ENV === 'production' ? config.domain! : '*'
+}));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send({
+        status: true,
+        message: 'Well Come To Setup Templete',
+    });
+});
+
+app.use('/api/v1', router)
 
 export default app;
---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import path from 'path';
+import HealthRouter from '../module/health/health.routes';
+import express from 'express';
+
+const router = express.Router()
+
+const moduleRoutes = [
+    { path: '/health', route: HealthRouter }
+]
+
+moduleRoutes.forEach((r) => router.use(r.path, r.route))
+export default router
